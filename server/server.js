aa1dc6f52843147598a79a0e88a5772afc72db9b
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-06-06 22:17:39 +0200

    Added express app

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const axios = require('axios');
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+const GOOGLEBOOKSAPI = 
+
+
+
+
+app.get('/books/:title', async (req, res)=>{
+    
+    console.log('Parameter', req.params.title);
+
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+req.params.title).then(response => {
+        console.log('Response', response.data.items)
+        res.json(response.data.items);
+
+    })
+
+})
+
+
+
+app.get("/", (req, res) => {
+  res.json("Root Page");
+});
+
+server.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}...`);
+});
