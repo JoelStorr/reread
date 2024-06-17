1ba02b70a334833f36086e7daa3e808d9e0efd1f
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-06-17 17:31:15 +0200

    clean up

--- server/util/db.js
+++ server/util/db.js
@@ -2,9 +2,7 @@ var connection = mysql.createConnection({
   host: process.env.host,
   user: process.env.user,
   password: process.env.dbpassword, // password for db.
-  database: process.env.dbname // db name.
+  database: process.env.dbname, // db name.
 });
 
-
 modules.export = connection;
-
