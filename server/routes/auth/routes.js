623582f434f5fe882cf85aaf373001e63584dfb3
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-06-26 22:25:02 +0200

    Added authentication routes

--- server/routes/auth/routes.js
+++ server/routes/auth/routes.js
@@ -11,4 +11,34 @@ authRouter.get("/signup", registerUser);
 authRouter.post("/signup/profileinfo", userProfileInfo);
 
 
+
+
+
+
+// Google Callbacks
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/failure",
+    successRedirect: "/",
+    session: false, //Diabled for testing without session
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
+
+authRouter.get("/failure", (req, res) => {
+  return res.send("Failed to login");
+});
+
+
+authRouter.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+
 module.exports = authRouter;
