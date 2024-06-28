df264a84eb45d4c39df69e76cefd2b40c1558dbe
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-06-28 21:31:16 +0200

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="/auth/google/callback"
-          
-          
-        >
+        <a className="App-link" href="/auth/google/callback">
           Learn React
         </a>
       </header>
