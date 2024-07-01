5f87cdd95916c582e3b378d74b42745cf6b5dada
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-07-01 18:50:56 +0200

    clean up

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
 const http = require("http");
 const path = require("path");
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = require("./app");
-
-
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-
 app.get("/profile", (req, res) => {
   res.json("Root Profile page");
 });
-
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
